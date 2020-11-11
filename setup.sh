a2fb55995c263ed7a004a6f44b5e57f7395edcb6
Nick Kuipers <nkuipers@f0r1s12.codam.nl>
2020-11-11 14:48:19 +0100

    update

--- setup.sh
+++ setup.sh
@@ -6,11 +6,15 @@
 #    By: nkuipers <nkuipers@student.codam.nl>         +#+                      #
 #                                                    +#+                       #
 #    Created: 2020/08/15 12:39:24 by nkuipers      #+#    #+#                  #
-#    Updated: 2020/08/15 12:40:07 by nkuipers      ########   odam.nl          #
+#    Updated: 2020/11/11 14:03:43 by nkuipers      ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
-minikube --vm-driver=docker start --extra-config=apiserver.service-node-port-range=1-35000
-minikube addons enable ingress
-minikube addons enable dashboard
-minikube dashboard &
+#!bin/zsh
+# create link in goinfre to save memory
+rm -rf ~/.minikube/machines
+mkdir -p ~/goinfre/machines
+ln -s ~/goinfre/machines ~/.minikube/machines
+minikube delete
+minikube start --driver=virtualbox
+
