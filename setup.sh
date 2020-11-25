4513d48ee1a9384a1bdcfdb36174ce3c3ee5bc40
Nick Kuipers <nkuipers@f0r1s12.codam.nl>
2020-11-25 12:16:48 +0100

    services

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 #    By: nkuipers <nkuipers@student.codam.nl>         +#+                      #
 #                                                    +#+                       #
 #    Created: 2020/08/15 12:39:24 by nkuipers      #+#    #+#                  #
-#    Updated: 2020/11/11 15:26:38 by nkuipers      ########   odam.nl          #
+#    Updated: 2020/11/18 11:35:17 by nkuipers      ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,7 +15,7 @@
 rm -rf ~/.minikube/machines
 mkdir -p ~/goinfre/machines
 ln -s ~/goinfre/machines ~/.minikube/machines
-# minikube delete
+minikube delete
 minikube start --driver=virtualbox
 minikube addons enable dashboard
 minikube addons enable metrics-server
@@ -26,5 +26,8 @@ eval $(minikube docker-env)
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml > /dev/null
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml > /dev/null
 kubectl apply -f srcs/metallb-config.yaml > /dev/null
+
+kubectl run nginx --image=nginx
+kubectl apply -f srcs/metallb-config.yaml > /pod/nginx
 
 minikube dashboard &
